6aeff73823dd8bbcfa937ef11fe4be6a1ed58b85
leonardo.giglio <giglioleonardo@hotmail.com>
2022-11-03 14:00:17 -0300

    Validacion Formulario

--- js/formulario.js
+++ js/formulario.js
@@ -6,10 +6,9 @@ propFormulario = {
 
     formulario: document.formulario_contacto,
     elementos: document.formulario_contacto.elements,
+    error: null,
+    textoError: null,
 }
-
-
-
 
 //Objeto con metodos de formulario
 
@@ -25,17 +24,47 @@ metFormulario = {
                 propFormulario.elementos[i].addEventListener('blur', metFormulario.blurInput);
             }  
         }
+
+        propFormulario.formulario.addEventListener('submit', metFormulario.validarInputs);
     },
 
-    focusInput : function () {
+    focusInput: function () {
 
         this.parentElement.children[1].className = 'label active';
     },
 
-    blurInput : function () {
+    blurInput: function () {
 
         if (this.value == '') {
             this.parentElement.children[1].className = 'label'; 
+        }
+    },
+
+    validarInputs: function (e) {
+        for (let i = 0; i < propFormulario.elementos.length; i++) {
+            
+            if( propFormulario.elementos[i].value == '' ) {
+
+                e.preventDefault();
+
+                if( propFormulario.elementos[i].parentElement.children.length < 3 ) {
+
+                    propFormulario.error = document.createElement('p');
+                    propFormulario.textoError = document.createTextNode('Por favor llena el campo con tu ' + propFormulario.elementos[i].name);
+                    propFormulario.error.appendChild(propFormulario.textoError);
+                    propFormulario.error.className = 'error';
+    
+                    propFormulario.elementos[i].parentElement.appendChild(propFormulario.error);
+                }
+
+            } else {
+
+                if( propFormulario.elementos[i].parentElement.children.length >= 3 ) {
+
+                    propFormulario.error = propFormulario.elementos[i].parentElement.getElementsByTagName('p')[0];
+                    propFormulario.elementos[i].parentElement.removeChild(propFormulario.error);
+                }
+            }
         }
     }
 }
